6a390776d7d4419a36cd0de090ab8c92f334fb95
hansonq <lcacbcac@gmail.com>
2017-10-03 20:58:55 +0200

    Initial commit

--- src/main/java/pl/hansonq/firstApplication/controllers/RegisterController.java
+++ src/main/java/pl/hansonq/firstApplication/controllers/RegisterController.java
@@ -2,16 +2,27 @@ package pl.hansonq.firstApplication.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class RegisterController {
-    @RequestMapping(value = "/register",method = RequestMethod.POST)
-    public String registerPost(@RequestParam("login")String login,
-                               @RequestParam("password")String password,
-                               @RequestParam("passwordRepeat")String passwordRepeat){
-        return password.equals(passwordRepeat) ? "Zarejestrowana poprawnie" : "Hasła nie sa takie same";
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String registerPost(@RequestParam("login") String login,
+                               @RequestParam("password") String password,
+                               @RequestParam("passwordRepeat") String passwordRepeat,
+                               Model model){
+        String text = password.equals(passwordRepeat) ? "Zarejestrowano poprawnie" : "Hasła nie sa takie same";
+        model.addAttribute("info", text);
+        return "register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String registerPost(){
+        return "register";
     }
 }
